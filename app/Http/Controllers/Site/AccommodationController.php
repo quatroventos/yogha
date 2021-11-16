28e0487d773c428829b719e948e8bc4323d36b94
Gabriel <contato@quatroventos.com.br>
2021-11-16 10:47:55 -0300

    Migration e Model para Rates Importação da tabela Rates

---
+++ app/Http/Controllers/Site/AccommodationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AccommodationController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index($accommodationid)
+    {
+        $accommodation = \DB::table('accommodations')
+            ->select('accommodations.*','descriptions.*')
+            ->join('descriptions','descriptions.AccommodationId','=','accommodations.AccommodationId')
+            ->where('accommodations.AccommodationId','=', $accommodationid)
+            ->get();
+
+        return view('site.accommodation.index', compact('accommodation'));
+    }
+
+}
---
+++ database/migrations/2021_11_16_130712_create_rates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('AccommodationId')->nullable();
+            $table->integer('Capacity')->nullable();
+            $table->longText('Rates')->nullable();
+            $table->text('VAT')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rates');
+    }
+}
