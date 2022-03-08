73ae90d8c24ba99df7cbdbd0742614ab6d60761c
Gabriel <contato@quatroventos.com.br>
2022-03-08 17:50:45 -0300

    Máscaras no checkout

--- resources/views/site/checkout/index.blade.php
+++ resources/views/site/checkout/index.blade.php
@@ -263,7 +263,7 @@
                                             <input class="d-flex" type="text" name="card_cvv" placeholder="CVV">
                                         </div>
                                         <div class="form-group">
-                                            <input class="d-flex" type="text" name="document" placeholder="Documento">
+                                            <input class="d-flex cpf" type="text" name="document" placeholder="Documento">
                                         </div>
                                         <div class="form-group">
                                             <input class="d-flex" type="text" name="email" placeholder="E-mail" value="{{$user->email}}">
@@ -313,7 +313,7 @@
                                         <input type="hidden" name="paymenttype" value="BOLETO">
 
                                         <div class="form-group">
-                                            <input class="d-flex" type="text" name="document" placeholder="CPF">
+                                            <input class="d-flex cpf" type="text" name="document" placeholder="CPF">
                                         </div>
                                         <div class="form-group">
                                             <input class="d-flex" type="text" name="email" placeholder="E-mail de cobrança" value="{{$user->email}}">
@@ -377,45 +377,15 @@
         </div>
     </div>
 </section>
-
-<script>
-    $(document).ready(function(){
-        $('.date').mask('00/00/0000');
-        $('.time').mask('00:00:00');
-        $('.date_time').mask('00/00/0000 00:00:00');
-        $('.cep').mask('00000-000');
-        $('.phone').mask('0000-0000');
-        $('.phone_with_ddd').mask('(00) 00000-0000');
-        $('.phone_us').mask('(000) 000-0000');
-        $('.mixed').mask('AAA 000-S0S');
-        $('.cpf').mask('000.000.000-00', {reverse: true});
-        $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
-        $('.money').mask('000.000.000.000.000,00', {reverse: true});
-        $('.money2').mask("#.##0,00", {reverse: true});
-        $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
-            translation: {
-                'Z': {
-                    pattern: /[0-9]/, optional: true
-                }
-            }
+    <!-- Masks -->
+    <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
+    <script>
+        $(document).ready(function(){
+            $('.date').mask('00/00');
+            $('.cpf').mask('000.000.000-00', {reverse: true});
+            $('.selectonfocus').mask("00/00", {selectOnFocus: true});
         });
-        $('.ip_address').mask('099.099.099.099');
-        $('.percent').mask('##0,00%', {reverse: true});
-        $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
-        $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
-        $('.fallback').mask("00r00r0000", {
-            translation: {
-                'r': {
-                    pattern: /[\/]/,
-                    fallback: '/'
-                },
-                placeholder: "__/__/____"
-            }
-        });
-        $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
-    });
-</script>
+    </script>
 
 @endsection
-
 
